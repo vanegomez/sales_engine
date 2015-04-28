c02e0275254d7068354afbd4606867266eafb6f8
vanegomez <vane.gomez@me.com>
2015-04-28 13:11:43 -0600

    customer test

---
+++ test/customer_test.rb
@@ -0,0 +1,42 @@
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "../lib/customer"
+
+class CustomerTest < Minitest::Test
+
+  def setup
+    data = {
+      id: "1",
+      first_name: "Joey",
+      last_name: "Ondricka",
+      created_at: "2012-03-27 14:54:09 UTC",
+      updated_at: "2012-03-27 14:54:09 UTC"
+    }
+    @customer = Customer.new(data, nil)
+  end
+
+  def test_it_has_an_id
+    @customer
+    assert_equal 1, @customer.id
+  end
+
+  def test_it_has_a_first_name
+    @customer
+    assert_equal "Joey", @customer.first_name
+  end
+
+  def test_it_has_a_last_name
+    @customer
+    assert_equal "Ondricka", @customer.last_name
+  end
+
+  def test_costumer_was_created_at
+    @customer
+    assert_equal "2012-03-27 14:54:09 UTC", @customer.created_at
+  end
+
+  def test_costumer_was_updated_at
+    @customer
+    assert_equal "2012-03-27 14:54:09 UTC", @customer.updated_at
+  end
+end
